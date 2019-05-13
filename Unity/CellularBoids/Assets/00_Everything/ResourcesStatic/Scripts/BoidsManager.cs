8dbdf6385c6be7b0d9e0e197bd22084b85457fe3
Eddie Lee <eddie@funktroniclabs.com>
2019-05-12 23:54:44 -0700

    more setup

--- Unity/CellularBoids/Assets/00_Everything/ResourcesStatic/Scripts/BoidsManager.cs
+++ Unity/CellularBoids/Assets/00_Everything/ResourcesStatic/Scripts/BoidsManager.cs
@@ -8,28 +8,61 @@ using UnityEngine.Jobs;
 
 public class BoidsManager : MonoBehaviour
 {
-    const int NumCells = 512;
+    const int NumCells = 4096;
+    const int NumGroups = 8;
+
     public GameObject PfbCell; // prototype
 
     Transform [] _cellTfms;
     TransformAccessArray _cellTfmAccessArray;
     GameObject [] _cellObjs;
+    MeshRenderer[] _cellRenderers;
+
+    NativeArray<int> _cellGroupIndex;
+    NativeArray<Vector3> _cellPositions;
     NativeArray<Vector3> _cellVelocities;
+    NativeArray<float> _cellGroupsForceMatrix;
 
     PositionUpdateJob _jobPos;
+    CellularForceJob _jobCellularForce;
     JobHandle _jobHandlePosition;
-    //JobHandle m_AccelJobHandle;
+    JobHandle _jobsHandleCellularForce;
+
+    struct CellularForceJob : IJobParallelFor
+    {
+        [ReadOnly] public NativeArray<Vector3> position;
+        public NativeArray<Vector3> velocity; 
+
+        [ReadOnly] public NativeArray<int> groupIndex;
+        [ReadOnly] public NativeArray<float> forceMatrix;
+        [ReadOnly] public int numCells;
+
+        public float deltaTime;
+
+        public void Execute(int i)
+        {
+            // apply force from all
+            Vector3 currVel = velocity[i];
+
+            currVel.x = 1f;
+            currVel.y = 0;
+            currVel.z = 0f;
+
+            velocity[i] = currVel;
+        }
+    }
 
     struct PositionUpdateJob : IJobParallelForTransform
     {
-        [ReadOnly]
-        public NativeArray<Vector3> velocity;  // the velocities from AccelerationJob
+        public NativeArray<Vector3> position;  // the velocities from AccelerationJob
+        [ReadOnly] public NativeArray<Vector3> velocity;  // the velocities from AccelerationJob
 
         public float deltaTime;
 
         public void Execute(int i, TransformAccess transform)
         {
-            transform.position += velocity[i] * deltaTime;
+            position[i] += velocity[i] * deltaTime;
+            transform.position = position[i];
         }
     }
 
@@ -40,14 +73,26 @@ public class BoidsManager : MonoBehaviour
 
     void Update()
     {
-        _jobPos = new PositionUpdateJob()
+        _jobCellularForce = new CellularForceJob()
         {
-            deltaTime = Time.deltaTime,
+            numCells = NumCells,
+            position = _cellPositions,
             velocity = _cellVelocities,
+            groupIndex = _cellGroupIndex,
+            forceMatrix = _cellGroupsForceMatrix,
+
+            deltaTime = Time.deltaTime,
         };
 
-        //m_AccelJobHandle = m_AccelJob.Schedule(m_ObjectCount, 64);
-        _jobHandlePosition = _jobPos.Schedule(_cellTfmAccessArray);
+        _jobPos = new PositionUpdateJob()
+        {
+            position = _cellPositions,
+            velocity = _cellVelocities,
+            deltaTime = Time.deltaTime,
+        };
+
+        _jobsHandleCellularForce = _jobCellularForce.Schedule(NumCells, 64);
+        _jobHandlePosition = _jobPos.Schedule(_cellTfmAccessArray, _jobsHandleCellularForce);
     }
 
     public void LateUpdate()
@@ -58,6 +103,9 @@ public class BoidsManager : MonoBehaviour
     private void OnDestroy()
     {
         _cellVelocities.Dispose();
+        _cellGroupIndex.Dispose();
+        _cellPositions.Dispose();
+        _cellGroupsForceMatrix.Dispose();
         _cellTfmAccessArray.Dispose();
     }
 
@@ -65,21 +113,37 @@ public class BoidsManager : MonoBehaviour
     {
         _cellObjs = new GameObject[NumCells];
         _cellTfms = new Transform[NumCells];
+        _cellRenderers = new MeshRenderer[NumCells];
+
+        _cellGroupIndex = new NativeArray<int>(NumCells, Allocator.Persistent);
         _cellVelocities = new NativeArray<Vector3>(NumCells, Allocator.Persistent);
+        _cellPositions = new NativeArray<Vector3>(NumCells, Allocator.Persistent);
 
         float radius = 10f;
 
         for( int i = 0; i < NumCells; ++i)
         {
-            var newCell = GameObject.Instantiate(PfbCell);
-            newCell.transform.position = new Vector3( UnityEngine.Random.Range(-radius, radius), UnityEngine.Random.Range(-radius, radius), UnityEngine.Random.Range(-radius, radius));
+            var newCellGroupIndex = UnityEngine.Random.Range(0, NumGroups);
 
+            var newCell = GameObject.Instantiate(PfbCell);
+
+            _cellPositions[i] = new Vector3( UnityEngine.Random.Range(-radius, radius), UnityEngine.Random.Range(-radius, radius), UnityEngine.Random.Range(-radius, radius));
             _cellObjs[i] = newCell;
             _cellTfms[i] = newCell.transform;
-
+            _cellGroupIndex[i] = newCellGroupIndex;
             _cellVelocities[i] = Vector3.up;
+            _cellRenderers[i] = newCell.GetComponent<MeshRenderer>();
+
+            //newCell Color.HSVToRGB((float)newCellGroupIndex / (float)NumGroups, 1f, 1f);
+        }
+        _cellTfmAccessArray = new TransformAccessArray(_cellTfms);
+
+        // force matrix
+        _cellGroupsForceMatrix = new NativeArray<float>(NumGroups * NumGroups, Allocator.Persistent);
+        for(int i = 0; i < _cellGroupsForceMatrix.Length; ++i)
+        {
+            _cellGroupsForceMatrix[i] = UnityEngine.Random.Range(-1f, 1f);
         }
 
-        _cellTfmAccessArray = new TransformAccessArray(_cellTfms);
     }
 }
