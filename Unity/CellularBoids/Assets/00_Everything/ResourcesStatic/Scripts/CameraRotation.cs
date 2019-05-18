fdc660f25dd3ea2afcc09942607ae5baa3e0dc5b
Eddie Lee <eddie@funktroniclabs.com>
2019-05-18 03:00:46 -0700

    more tweakz

---
+++ Unity/CellularBoids/Assets/00_Everything/ResourcesStatic/Scripts/CameraRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    float radius = 1f;
+
+    void Start()
+    {
+        radius = transform.position.magnitude;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Time.time*0.2f;
+        transform.position = new Vector3( radius * Mathf.Cos(angle), 0f, radius *Mathf.Sin(angle));
+
+        transform.LookAt(Vector3.zero);
+
+    }
+}
